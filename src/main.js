cc1d018d3d5bdbe64e7e6ac708a84614c7ef77bf
mohammedgodil <mohammed.godil@student.howest.be>
2025-01-23 17:22:14 +0100

    update path

--- src/main.js
+++ src/main.js
@@ -464,7 +464,7 @@ function animateStampOnPaper() {
 
   setTimeout(() => {
     stamp.style.display = "none";
-    normal_paper.src = "./integration3/src/assets/printed_paper.png";
+    normal_paper.src = "./src/assets/printed_paper.png";
     normal_paper.style.transform = "scale(1.2)";
     printed_paper_section.style.paddingBottom = "2rem";
     normal_paper.style.paddingTop = "1.5rem";
@@ -482,7 +482,7 @@ resetButton.addEventListener("click", () => {
   stamp.style.animation = "";
   resetButton.style.display = "none";
 
-  normal_paper.src = "./integration3/src/assets/normal_paper.png";
+  normal_paper.src = "./src/assets/normal_paper.png";
   normal_paper.style.transform = "";
   printed_paper_section.style.paddingBottom = "";
   normal_paper.style.paddingTop = "";
