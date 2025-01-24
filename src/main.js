9facf432e95005f76f3b76bf2ad8d1574d84401c
mohammedgodil <mohammed.godil@student.howest.be>
2025-01-24 09:11:34 +0100

    hamburger menu and word highlight

--- src/main.js
+++ src/main.js
@@ -921,3 +921,57 @@ function goPrevPage() {
     currentLocation--;
   }
 }
+
+const init = () => {
+  const $nav = document.querySelector(".nav");
+  const $navButton = document.querySelector(".nav__button");
+  const $navList = document.querySelector(".nav__list");
+  const $iconLink = document.querySelector("#iconlink");
+  const listItems = $navList.querySelectorAll("li a");
+  const closing__button = document.querySelector(".closing__button");
+
+  $navButton.classList.remove("hidden");
+  // $navList.classList.add("hidden");
+
+  const openNavigation = () => {
+    $navButton.setAttribute("aria-expanded", "true");
+    // $iconLink.setAttribute("xlink:href", "#close");
+    $navList.classList.add("abc");
+  };
+
+  const closeNavigation = () => {
+    $navButton.setAttribute("aria-expanded", "false");
+    // $iconLink.setAttribute("xlink:href", "#navicon");
+    $navList.classList.remove("abc");
+  };
+
+  const toggleNavigation = () => {
+    const open = $navButton.getAttribute("aria-expanded");
+    open === "false" ? openNavigation() : closeNavigation();
+  };
+
+  closing__button.addEventListener("click", () => {
+    $navList.classList.remove("abc");
+  });
+
+  const handleBlur = () => {
+    //if (!event.relatedTarget || !$navList.contains(event.relatedTarget)) {
+    closeNavigation();
+    //}
+  };
+
+  $navButton.addEventListener("click", toggleNavigation);
+
+  // add event to the last item in the nav list to trigger the disclosure to close if the user tabs out of the disclosure
+  listItems[listItems.length - 1].addEventListener("blur", handleBlur);
+
+  // Close the disclosure if a user presses the escape key
+  window.addEventListener("keyup", (e) => {
+    if (e.key === "Escape") {
+      $navButton.focus();
+      closeNavigation();
+    }
+  });
+};
+
+init();
