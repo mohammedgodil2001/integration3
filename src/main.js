1f1eadaf397c29ddfd317db3336038fecf54e4ff
mohammedgodil <mohammed.godil@student.howest.be>
2025-01-25 03:08:50 +0100

    paper debugging with javascript

--- src/main.js
+++ src/main.js
@@ -468,7 +468,7 @@ function animateStampOnPaper() {
   setTimeout(() => {
     stamp.style.display = "none";
     // normal_paper.src = "/printed_paper.png";
-    normal_paper.src = normal_paper.getAttribute("data-printed-src");
+    // normal_paper.src = normal_paper.getAttribute("data-printed-src");
     normal_paper.style.transform = "scale(1.2)";
     printed_paper_section.style.paddingBottom = "2rem";
     normal_paper.style.paddingTop = "1.5rem";
@@ -486,8 +486,8 @@ resetButton.addEventListener("click", () => {
   stamp.style.animation = "";
   resetButton.style.display = "none";
 
-  // normal_paper.src = "/normal_paper.png";
-  normal_paper.src = normal_paper.getAttribute("data-normal-src");
+  normal_paper.src = "/normal_paper.png";
+  // normal_paper.src = normal_paper.getAttribute("data-normal-src");
   normal_paper.style.transform = "";
   printed_paper_section.style.paddingBottom = "";
   normal_paper.style.paddingTop = "";
